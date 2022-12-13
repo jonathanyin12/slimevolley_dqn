2e80fa4ac587dc75d9cbdda7a2056ec8ce4614f7
Jonathan <jonathanyin.12@gmail.com>
2022-12-13 16:39:31 -0500

    add slimevolleygym

--- test_model.py
+++ test_model.py
@@ -27,34 +27,36 @@ def custom_reward(obs):
     opp_y = obs[9]
 
     # reward for hitting the ball up
-    if ball_x > 0 and ball_y_vel >=0:
+    if ball_x > 0 and ball_y_vel >= 0:
         hit += 1/(abs(agent_x-ball_x) + abs(agent_y-ball_y))
 
     # reward for hitting ball forward
-    if ball_x > 0 and ball_x_vel <=0:
+    if ball_x > 0 and ball_x_vel <= 0:
         hit += 1/(abs(agent_x-ball_x) + abs(agent_y-ball_y))
-    
+
     # # reward based on x dist
     if ball_x > 0:
-        dist -=abs(agent_x-ball_x)
+        dist -= abs(agent_x-ball_x)
     if ball_x < 0:
         dist += abs(opp_x+ball_x)
 
     return hit+dist
 
+
 def getAction(i):
     if i == 0:
-        return [1,0,0]
+        return [1, 0, 0]
     elif i == 1:
-        return [1,0,1]
+        return [1, 0, 1]
     elif i == 2:
-        return [0,1,0]
+        return [0, 1, 0]
     elif i == 3:
-        return [0,1,1]
+        return [0, 1, 1]
     elif i == 4:
-        return [0,0,1]
+        return [0, 0, 1]
     elif i == 5:
-        return [0,0,0]
+        return [0, 0, 0]
+
 
 def policy(model, obs):
     current_state_tensor = tf.convert_to_tensor(obs)
@@ -64,13 +66,53 @@ def policy(model, obs):
     action = getAction(action_index)
     return action
 
-if __name__=="__main__":
-    
 
+if __name__ == "__main__":
+    """
+    Example of how to use Gym env, in single or multiplayer setting
+
+    Humans can override controls:
+
+    blue Agent:
+    W - Jump
+    A - Left
+    D - Right
+
+    Yellow Agent:
+    Up Arrow, Left Arrow, Right Arrow
+    """
     from pyglet.window import key
     from time import sleep
 
-    model = keras.models.load_model("model_epoch150_large.h5", compile=False)
+    manualAction = [0, 0, 0] # forward, backward, jump
+    otherManualAction = [0, 0, 0]
+    manualMode = False
+    otherManualMode = False
+
+    # taken from https://github.com/openai/gym/blob/master/gym/envs/box2d/car_racing.py
+    def key_press(k, mod):
+        global manualMode, manualAction, otherManualMode, otherManualAction
+        if k == key.LEFT:  manualAction[0] = 1
+        if k == key.RIGHT: manualAction[1] = 1
+        if k == key.UP:    manualAction[2] = 1
+        if (k == key.LEFT or k == key.RIGHT or k == key.UP): manualMode = True
+
+        if k == key.D:     otherManualAction[0] = 1
+        if k == key.A:     otherManualAction[1] = 1
+        if k == key.W:     otherManualAction[2] = 1
+        if (k == key.D or k == key.A or k == key.W): otherManualMode = True
+
+    def key_release(k, mod):
+        global manualMode, manualAction, otherManualMode, otherManualAction
+        if k == key.LEFT:  manualAction[0] = 0
+        if k == key.RIGHT: manualAction[1] = 0
+        if k == key.UP:    manualAction[2] = 0
+        if k == key.D:     otherManualAction[0] = 0
+        if k == key.A:     otherManualAction[1] = 0
+        if k == key.W:     otherManualAction[2] = 0
+
+
+    model = keras.models.load_model("model_epoch50.h5", compile=False)
     print(model.summary())
     base_policy = slimevolleygym.BaselinePolicy() # defaults to use RNN Baseline for player
     
@@ -78,26 +120,37 @@ if __name__=="__main__":
     env.seed(np.random.randint(0, 10000))
 
     env.render()
+    env.viewer.window.on_key_press = key_press
+    env.viewer.window.on_key_release = key_release
 
    
 
-    for i in range(1):
-        obs1 = env.reset()
-        obs2 = obs1 # both sides always see the same initial observation.
+    obs1 = env.reset()
+    obs2 = obs1 # both sides always see the same initial observation.
 
-        done = False
-        total_reward = 0
-        while not done:
-
-            action1 = policy(model, obs1)
+    done = False
+    total_reward = 0
+    while not done:
+        if otherManualMode: # override with keyboard
+            action2 = otherManualAction
+        else:
             action2 = base_policy.predict(obs2)
-            # print(custom_reward(obs1))
-            obs1, reward, done, info = env.step(action1, action2) # extra argument
-            obs2 = info['otherObs']
 
-            total_reward += reward
-            env.render()
-            sleep(0.02) # 0.01
+
+        if manualMode:
+            action1 = manualAction
+        else:
+            action1 = policy(model, obs1)
+
+
+        # print(custom_reward(obs1))
+        obs1, reward, done, info = env.step(action1,action2) # extra argument
+        obs2 = info['otherObs']
+
+        total_reward += reward
+
+        env.render()
+        sleep(0.02) # 0.01
 
     env.close()
     print("cumulative score", total_reward)
